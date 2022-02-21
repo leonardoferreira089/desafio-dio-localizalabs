548d0fb3ca8f1d3e56f4c6cbe6c6fedea92443b9
Leonardo Ferreira <leonardo.ferreira089@gmail.com>
2022-02-21 22:01:27 +0100

    Desafio Github LocalizaLabs

---
+++ Desafio05.DIO/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Desafio05.DIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string joia = "";
+            bool fim = false;
+            List<string> joias = new List<string>();
+            while (!fim)
+            {
+                joia = Console.ReadLine();
+                fim = string.IsNullOrWhiteSpace(joia);
+                if (!fim)
+                {
+                    joias.Add(joia);
+                }
+            }
+
+            IEnumerable<string> distinctJoias = joias.Distinct();
+            Console.WriteLine(distinctJoias.Count());
+        }
+    }
+}
